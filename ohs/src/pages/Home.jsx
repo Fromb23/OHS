dde1c1047ea41f02dfb0f7fd18545d9bae9d8ef2
Fromb23 <rombo.programming@gmail.com>
2025-02-15 02:12:58 +0300

    config tailwind

---
+++ ohs/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+	return (
+	  <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-900">
+		<h1 className="text-4xl font-bold text-red-600">Welcome to the Home Page</h1>
+		<p className="mt-2 text-lg text-gray-700">This is a Tailwind-styled component.</p>
+	  </div>
+	);
+  }
+  
+  export default Home;
+  
