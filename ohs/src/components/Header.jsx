f72490d26e70b2dfa69254ff1eca1b3c948cbacf
Fromb23 <rombo.programming@gmail.com>
2025-02-15 03:22:20 +0300

    header

--- ohs/src/components/Header.jsx
+++ ohs/src/components/Header.jsx
@@ -4,10 +4,10 @@ import logo from '../assets/ohs_logo.jpg';
 const Header = () => {
   return (
 	<div className="fixed top-0 left-0 w-full bg-[#800000]">
-		<div className="h-45">
-			<img className="w-20 p-3 h-10 sm:w-30 sm:h-33 object-contain bg-[#800000]" src={logo} alt="OHS Logo"/>
-			<div className="flex justify-center items-center h-10"> 
-				<h1 className="flex-1 text-2xl sm:text-4xl font-bold text-white text-center">OLYMPIC HIGH SCHOOL</h1>
+		<div className="sm:h-45 p-3 h-22">
+			<img className="p-2 h-18 sm:w-29 sm:h-33 object-contain bg-[#800000] rounded-5" src={logo} alt="OHS Logo"/>
+			<div className="flex sm:justify-start sm:items-center sm:h-19 sm:w-full ">
+				<h1 className="-mt-15 ml-13 sm:w-1/2 sm:h-50 text-lg sm:text-4xl font-bold text-white text-center ml:[29%]">OLYMPIC HIGH SCHOOL</h1>
 			</div>
 		</div>
 	</div>
